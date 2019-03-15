de6b2fae82e182cc831ca1de62304a12d7d77ba8
Timothy Steward <tsteward0@gmail.com>
2019-03-15 13:09:04 +0800

    fix error and format

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
 import 'package:angular/angular.dart';
-import 'package:angular_scripture_card_generator/app_component.template.dart' as ng;
+import 'package:scripture_card_generator/app_component.template.dart' as ng;
 
 void main() {
   runApp(ng.AppComponentNgFactory);
